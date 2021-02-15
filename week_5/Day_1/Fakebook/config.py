9c146a9e1a76581c58665de70eb80e4096b682ff
alex <alexrt3@gmail.com>
2021-02-15 00:06:33 -0600

    commit

--- week_5/Day_1/Fakebook/config.py
+++ week_5/Day_1/Fakebook/config.py
@@ -1,7 +1,7 @@
-import os 
+import os
 from dotenv import load_dotenv
 
-basedir = os.path.abspath(os.dirname(__name__))
+basedir = os.path.abspath(os.path.dirname(__name__))
 load_dotenv(os.path.join(basedir, '.env'))
 
 class Config:
