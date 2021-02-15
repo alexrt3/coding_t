39d2f7a28f03d3283baa5febeb1d8b99f7590d6a
alex <alexrt3@gmail.com>
2021-02-14 21:15:12 -0600

    commit master

--- week_5/Day_1/Fakebook/app/views.py
+++ week_5/Day_1/Fakebook/app/views.py
@@ -13,3 +13,11 @@ def profile():
 @app.route('/contact')
 def contact():
     return render_template('contact.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/register')
+def register():
+    return render_template('register.html')
