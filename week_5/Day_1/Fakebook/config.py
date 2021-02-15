bdf5a5ad9a54ff93f1d67994e5955533596510e4
alex <alexrt3@gmail.com>
2021-02-15 00:09:02 -0600

    commit

--- week_5/Day_1/Fakebook/config.py
+++ week_5/Day_1/Fakebook/config.py
@@ -4,6 +4,7 @@ from dotenv import load_dotenv
 basedir = os.path.abspath(os.path.dirname(__name__))
 load_dotenv(os.path.join(basedir, '.env'))
 
+
 class Config:
     SQLALCHEMY_DATABASE_URI = os.getenv('SQLALCHEMY_DATABASE_URI')
     SQLALCHEMY_TRACK_MODIFICATIONS = os.getenv('SQLALCHEMY_TRACK_MODIFICATIONS')
