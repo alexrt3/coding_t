062a15303ba00f24ad5b36865a2aeaa6cd7c15ae
alex <alexrt3@gmail.com>
2021-02-15 00:55:04 -0600

    commit master

---
+++ week_5/Day_1/Fakebook/app/models.py
@@ -0,0 +1,14 @@
+from app import db
+
+#Object Relationship Mapper
+#Flask-SQLAlchemy
+
+class User(db.Model):
+    user_id = db.Column(db.Integer, pimary_key=True)
+    first_name = db.Column(db.String)
+    last_name = db.Column(db.String)
+    email = db.Column(db.String, default=f'{first_name}{last_name}@codingtemple.com')
+    password = db.Column(db.String)
+
+def __repr__(self):
+    return f'<User: {self.email}>'
