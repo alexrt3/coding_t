509e18d1cbc8d547ff5f682301912af381b10461
alex <alexrt3@gmail.com>
2021-02-15 21:47:12 -0600

    commit

--- week_5/Day_1/MyBlog/app/views.py
+++ week_5/Day_1/MyBlog/app/views.py
@@ -4,3 +4,19 @@ from flask import render_template
 @app.route('/')
 def home():
     return render_template('home.html')
+
+@app.route('/profile')
+def profile():
+    return render_template('profile.html')
+
+@app.route('/explore')
+def explore():
+    return render_template('explore.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/register')
+def register():
+    return render_template('register.html')
