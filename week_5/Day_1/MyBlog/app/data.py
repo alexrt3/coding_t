5fd6a18517c2fe716b8878f0bfe920fe9139dc8b
alex <alexrt3@gmail.com>
2021-02-15 22:00:49 -0600

    commit

---
+++ week_5/Day_1/MyBlog/app/data.py
@@ -0,0 +1,17 @@
+post = [
+    {
+        'id':1,
+        'title': 'First Post',
+        'body': 'This is amazing!'
+    },
+    {
+        'id':2,
+        'title': 'Second Post',
+        'body': 'This is not thatamazing!'
+    },
+    {
+        'id':3,
+        'title': 'Third Post',
+        'body': 'Meh!'
+    },
+]
