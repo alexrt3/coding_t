bde98eb409d785e52aacf44a00caf08d964ccbd5
alex <alexrt3@gmail.com>
2021-02-15 23:09:38 -0600

    commit

--- week_5/Day_1/MyBlog/app/models.py
+++ week_5/Day_1/MyBlog/app/models.py
@@ -4,8 +4,15 @@ class User(db.Model):
     user_id = db.Column(db.Integer, primary_key=True)
     first_name = db.Column(db.String)
     last_name = db.Column(db.String)
-    email = db.Column(db.String, default=f'{first_name}{last_name}@codingtemple.com')
+    email = db.Column(db.String)
     password = db.Column(db.String)
+
+    def __init__(self, first_name, last_name, password):
+        super().__init__()
+        self.first_name = first_name
+        self.last_name = last_name
+        self.password = password
+        self.email = f'{self.first_name}{self.last_name[0]}@codingtemple.com'.lower()
 
     def __repr__(self):
         return f'<User: {self.email}>'
