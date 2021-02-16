45883686d7dd4f33980131fdbb01f39ecc4e5a57
alex <alexrt3@gmail.com>
2021-02-15 22:38:19 -0600

    commit all

--- week_5/Day_1/MyBlog/config.py
+++ week_5/Day_1/MyBlog/config.py
@@ -6,3 +6,4 @@ load_dotenv(os.path.join(basedir, '.env'))
 
 class Config:
     SQLALCHEMY_DATABASE_URI = os.getenv('SQLALCHEMY_DATABASE_URI')
+    SQLALCHEMY_TRACK_MODIFICATIONS = os.getenv('SQLALCHEMY_TRACK_MODIFICATIONS')
